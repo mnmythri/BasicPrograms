13f2c4256fbf01c106ee7335a1751677734ff5ef
user.name <user.email>
2020-04-21 18:40:25 +0530

    first commit

--- src/day8/Interface.java
+++ src/day8/Interface.java
@@ -1,10 +1,47 @@
 package day8;
 
-public class Interface {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	import java.util.*; 
+	import java.lang.*; 
+	import java.io.*; 
+	   
+	interface one 
+	{ 
+	    public void print_geek(); 
+	} 
+	   
+	interface two 
+	{ 
+	    public void print_for(); 
+	} 
+	   
+	interface three extends one,two 
+	{ 
+	    public void print_geek(); 
+	} 
+	class child implements three 
+	{ 
+	    @Override
+	    public void print_geek() { 
+	        System.out.println("Geeks"); 
+	    } 
+	   
+	    public void print_for() 
+	    { 
+	        System.out.println("for"); 
+	    } 
+	} 
+	  
+	// Drived class 
+	public class Interface {
+	{ 
+	    public static void main(String[] args) 
+	    { 
+	        child c = new child(); 
+	        c.print_geek(); 
+	        c.print_for(); 
+	        c.print_geek(); 
+	    } 
+	} 
+	
 
 }
